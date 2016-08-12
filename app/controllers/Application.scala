1ddd62af062229d232cdd029e58316d5aab3c7fc
Thilo Planz <thilo@cpan.org>
2016-08-12 21:14:33 +0800

    Make the controller a class as per Play 2.4 routing
    avoids compile errors like "type Application is not a member of package
    controllers", see http://stackoverflow.com/a/30544381/14955

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.libs.json._
 import play.api.mvc._
 import models.Book._
 
-object Application extends Controller {
+class Application extends Controller {
 
   def listBooks = Action {
     Ok(Json.toJson(books))
